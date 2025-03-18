babb7f612dae70110609526af5ca17e5e74809b5
ASHWINI-NAIK19 <62827572+ASHWINI-NAIK19@users.noreply.github.com>
2025-03-18 23:14:19 +0530

    areaandcircumferenceofcircle

--- area and circumference of circle.py
+++ area and circumference of circle.py
@@ -1,8 +1,11 @@
+'''Area=3.14*r*r
+Circumference=2*3.14*r'''
+
 radius=float(input("Enter radius:"))
 area=3.14*radius**2
-print("area of circle:",area)
+print("area of circle:",round(area,2))
 circumference=2*3.14*radius
-print("circumference of circle:",circumference)
+print("circumference of circle:",round(circumference,2))
 
 
 
